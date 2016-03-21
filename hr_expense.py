f0ba17bb2bde6093454c5cf91d87d21939b9573c
Valentin Thirion <valentinthirion@gmail.com>
2016-03-21 16:45:42 +0100

    Migrated view and model structure from 8.0 to 9.0

--- hr_expense.py
+++ hr_expense.py
@@ -3,5 +3,5 @@ from openerp import models, fields
 
 class AnalyticAccount(models.Model):
     # Add attachments to Analytic Accounts
-    _inherit = 'hr.expense.expense'
+    _inherit = 'hr.expense'
     attachments_ids = fields.Many2many('ir.attachment', string="Attached documents")
