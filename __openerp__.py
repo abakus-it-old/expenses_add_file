9ad2e700b2d32b61bbe3bf2c6f305dbf69801e17
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 15:05:50 +0200

    update version to 9

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Add file on expenses",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'depends': ['hr_expense'],
     'author': "AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
